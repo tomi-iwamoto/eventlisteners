12619a0c238f8033256213c23d45433e368ed398
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-03-05 15:22:28 -0500

    Added notes about event delegation

--- script.js
+++ script.js
@@ -2,27 +2,27 @@ const grandparent = document.querySelector('.grandparent');
 const parent = document.querySelector('.parent');
 const child = document.querySelector('.child');
 
-// grandparent.addEventListener('click', function(e) {
+/*grandparent.addEventListener('click', function(e) {
 
-//     console.log(e);
-//     //Will show me what has been targetted (in the console)
-//     console.log(e.target);
+     console.log(e);
+     //Will show me what has been targetted (in the console)
+     console.log(e.target);
 
-// })
+ })
 
 
-// //I can also add multiple event listeners to an element in the order that's been defined
-// grandparent.addEventListener('click', function(e) {
+I can also add multiple event listeners to an element in the order that's been defined
+grandparent.addEventListener('click', function(e) {
 
-//    console.log('word one')
+    console.log('word one')
 
-// })
+ })
 
-// grandparent.addEventListener('click', function(e) {
+ grandparent.addEventListener('click', function(e) {
 
-//    console.log('word two')
+    console.log('word two')
 
-// })
+ })*/
 
 
 
@@ -31,77 +31,145 @@ to going to the furthest---In the below case, when I click on the child
 element it technically means that all elements have been clicked because 
 the child element is inside of all of the elements--------*/
 //EVENT BUBBLING GOES UPWARD
-// grandparent.addEventListener('click', function() {
+/*grandparent.addEventListener('click', function() {
 
-//     console.log('grandparent');
+     console.log('grandparent');
     
-// })
+ })
 
-// parent.addEventListener('click', function() {
+ parent.addEventListener('click', function() {
 
-//     console.log('parent');
+     console.log('parent');
 
-// })
+ })
 
-// child.addEventListener('click', function() {
+ child.addEventListener('click', function() {
 
-//     console.log('child');
+     console.log('child');
 
-// })
+ })*/
 
 
 
 /*---------CAPTURING-------------*/
-//GOES FROM OUTSIDE TO INSIDE
-//This makes the grandparent in the "capture" phase
-/*This prints out "grandparent" first and then, goes 
+/*GOES FROM OUTSIDE TO INSIDE
+This makes the grandparent in the "capture" phase
+This prints out "grandparent" first and then, goes 
 back to, child, parent, grandparent because it captured
 the grandparent, but the rest does not have a capture event
 so it goes default to bubbling*/
 
-grandparent.addEventListener('click', function() {
+/*grandparent.addEventListener('click', function() {
 
-    console.log('grandparent capture');
+     console.log('grandparent capture');
     
-}, {capture: true });
+ }, {capture: true });
 
 
 
-grandparent.addEventListener('click', function() {
+ grandparent.addEventListener('click', function() {
 
-    console.log('grandparent bubble');
+     console.log('grandparent bubble');
     
-});
+ });
 
-parent.addEventListener('click', function(e) {
+ parent.addEventListener('click', function(e) {
 
-    //This stops from all bubbling or capturing phases to occur after this parent
-    e.stopPropagation();
-    console.log('parent capture');
+     //This stops from all bubbling or capturing phases to occur after this parent
+     e.stopPropagation();
+     console.log('parent capture');
     
-}, {capture: true });
+ }, {capture: true });
 
 
 
-parent.addEventListener('click', function() {
+ parent.addEventListener('click', function() {
 
-    console.log('parent bubble');
+     console.log('parent bubble');
     
-});
+ });
+
+ child.addEventListener('click', function() {
+
+     console.log('child capture');
+    
+ }, {capture: true });
+
+
+
+ child.addEventListener('click', function() {
+
+     console.log('child bubble');
+    
+ });*/
+
+
+/*----------ONE TIME EVENTS----------*/
+
+
+/*grandparent.addEventListener('click', function() {
+
+     console.log('grandparent bubble');
+    
+ });
+
+ parent.addEventListener('click', function() {
+
+     console.log('parent bubble');
+    
+     /*This will make it so that the parent element will fire
+     an event one time, but when clicked the next time it will 
+     not happen
+}, { once: true });
 
 child.addEventListener('click', function() {
 
-    console.log('child capture');
+     console.log('child bubble');
     
-}, {capture: true });
+ });*/
+
+/*-----------DELEGATING EVENTS------------*/
+
+/*const divs = document.querySelectorAll('div');
+
+ divs.forEach(div => {
+
+     div.addEventListener("click", function () {
+
+         console.log('hi');
+
+     })
+ })
+
+/*If I click on the below new element, the event listener
+created above will NOT work because the selector above (const divs = document...)
+was ran before we created this new div below- the new created element
+was added after the event listener was created*/
+/*const newDiv = document.createElement('div');
+ newDiv.style.width = "500px";
+ newDiv.style.height = "500px";
+ newDiv.style.backgroundColor = "blue";
+ document.body.append(newDiv);*/
+
+/*You can always add an additional event listener to the new created div but it can be unnecessary
+and cluncky. In order to make sure that all of the divs get an event listener, you can do the following:*/
 
 
+const divs = document.querySelectorAll('div');
 
-child.addEventListener('click', function() {
+document.addEventListener("click", function (e) {
 
-    console.log('child bubble');
-    
-});
+    //If the clicked element matches the element difined below, the event will fire. 
+    if (e.target.matches("div")) {
 
+        console.log("Hello");
 
+    }
+})
+
+const newDiv = document.createElement('div');
+newDiv.style.width = "500px";
+newDiv.style.height = "500px";
+newDiv.style.backgroundColor = "blue";
+document.body.append(newDiv);
 
